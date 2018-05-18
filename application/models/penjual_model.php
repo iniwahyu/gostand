b8630d52cd9a6874539001e2d25a229be7d621dc
abdielreyhan <abdielreyhan98@gmail.com>
2018-05-18 21:16:24 +0700

    tampil produk (belum selesai)

--- application/models/penjual_model.php
+++ application/models/penjual_model.php
@@ -10,6 +10,10 @@ class penjual_model extends CI_Model
         $res = $this->db->insert($table, $data); // Kode ini digunakan untuk memasukan record baru kedalam sebuah tabel
         return $res; // Kode ini digunakan untuk mengembalikan hasil $res
     }
+    public function tampilproduk($tabel,$username){
+		$res=$this->db->get_where($table,array('nama_toko=>$username'));//memilih tabel
+		return $res->result_array();//mengembalikan hasil
+    }
 	
 }
 	
